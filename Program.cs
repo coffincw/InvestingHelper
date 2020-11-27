6883b5242007202ebb4be98385165086a13dc130
Caleb Coffin <coffincw@gmail.com>
2020-11-27 01:58:12 -0500

    Complete get request

--- Program.cs
+++ Program.cs
@@ -2,50 +2,45 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ThreeFourteen.Finnhub.Client;
 
 namespace InvestingProject
 {
     class Program
     {
         string FINNHUB_API_TOKEN;
+
+        readonly string ROOT_ENDPOINT;
+
+        HttpClient client;
         public Program() {
             FINNHUB_API_TOKEN = Environment.GetEnvironmentVariable("FINNHUB_API_TOKEN");
+            client = new HttpClient();
+            ROOT_ENDPOINT = "https://finnhub.io/api/v1/";
         }
 
-        private async Task getCompanyProfile(string Ticker) {
-
-            string uri = "";
-            string url = "https://finnhub.io/api/stock/profile?symbol=" + Ticker + "&token=" + FINNHUB_API_TOKEN;
-
-            var client = new FinnhubClient(FINNHUB_API_TOKEN);
-            Console.WriteLine(await client.Stock.GetCompany(Ticker));
+        private async Task<JObject> getCompanyProfile(string Ticker) {
             
-            // string test = "https://jsonplaceholder.typicode.com/users";
+            string url = ROOT_ENDPOINT + "stock/profile2?symbol=" + Ticker + "&token=" + FINNHUB_API_TOKEN;
 
-            // Console.WriteLine("Making API Call...");
-            // HttpClient req = new HttpClient();
-            // var content = await req.GetAsync(test);
-            // Console.WriteLine(await content.Content.ReadAsStringAsync());
-            // HttpClientHandler handler = new HttpClientHandler()
-            // {
-            //     AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-            // };        
-            // HttpClient client = new HttpClient(handler);
-            // //client.BaseAddress = new Uri(uri);
-            // HttpResponseMessage response = client.GetAsync(url).Result;
-            // response.EnsureSuccessStatusCode();
-            // string result = response.Content.ReadAsStringAsync().Result;
-            // Console.WriteLine("Result: " + result);
+            Console.WriteLine("Making API Call...");
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string jsonString = await response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(jsonString);
+            return json;
 
         }
 
         public static async Task Main (string [] args) {
             Program main = new Program();
 
-            await main.getCompanyProfile("AAPL");
-            // Console.WriteLine(resp);
+            JObject json = await main.getCompanyProfile("AAPL");
+            Console.WriteLine(json);
         
         }
     }
