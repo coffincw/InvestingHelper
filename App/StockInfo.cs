c59a004f5f5d4d8ed659d644ce1534491c3cb27f
Caleb Coffin <coffincw@gmail.com>
2020-11-27 03:03:56 -0500

    Redo project structure to include winforms

--- App/StockInfo.cs
+++ App/StockInfo.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.IO;
-using System.Net;
+using System;
 using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using ThreeFourteen.Finnhub.Client;
+using System.Threading.Tasks;
 
-namespace InvestingProject
+namespace App
 {
-    class Program
+
+    class StockInfo
     {
+            
         string FINNHUB_API_TOKEN;
 
         readonly string ROOT_ENDPOINT;
 
         HttpClient client;
-        public Program() {
+
+
+        public StockInfo() {
             FINNHUB_API_TOKEN = Environment.GetEnvironmentVariable("FINNHUB_API_TOKEN");
             client = new HttpClient();
             ROOT_ENDPOINT = "https://finnhub.io/api/v1/";
@@ -34,14 +33,14 @@ namespace InvestingProject
             var json = JObject.Parse(jsonString);
             return json;
 
+            // Program main = new Program();
+
+            // JObject json = await main.getCompanyProfile("AAPL");
+            // Console.WriteLine(json);
+
         }
 
-        public static async Task Main (string [] args) {
-            Program main = new Program();
 
-            JObject json = await main.getCompanyProfile("AAPL");
-            Console.WriteLine(json);
-        
-        }
+
     }
 }
