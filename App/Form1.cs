80a9fae3a4c75ce573c0becbec1889fa5ee4be0b
Caleb Coffin <coffincw@gmail.com>
2020-11-27 03:07:04 -0500

    Add buttons and change form title

--- App/Form1.cs
+++ App/Form1.cs
@@ -15,6 +15,42 @@ namespace App
         public Form1()
         {
             InitializeComponent();
+            Form1_Load();
+        }
+
+        private void Form1_Load() {
+            // Creating and setting the properties of label 
+        Label l = new Label(); 
+        l.AutoSize = true; 
+        l.Text = "Do you want to submit this project?"; 
+        l.Location = new Point(222, 145); 
+        //l.Font = new Font("French Script MT", 18); 
+        // Adding this label to form 
+        this.Controls.Add(l); 
+  
+        // Creating and setting the properties of Button 
+        Button Mybutton = new Button(); 
+        Mybutton.Location = new Point(225, 198); 
+        Mybutton.Text = "Submit"; 
+        Mybutton.AutoSize = true; 
+        Mybutton.BackColor = Color.LightBlue; 
+        Mybutton.Padding = new Padding(6); 
+        //Mybutton.Font = new Font("French Script MT", 18); 
+  
+        // Adding this button to form 
+        this.Controls.Add(Mybutton); 
+  
+        // Creating and setting the properties of Button 
+        Button Mybutton1 = new Button(); 
+        Mybutton1.Location = new Point(360, 198); 
+        Mybutton1.Text = "Cancel"; 
+        Mybutton1.AutoSize = true; 
+        Mybutton1.BackColor = Color.LightPink; 
+        Mybutton1.Padding = new Padding(6); 
+        //Mybutton1.Font = new Font("French Script MT", 18); 
+  
+        // Adding this button to form 
+        this.Controls.Add(Mybutton1); 
         }
 
     }
